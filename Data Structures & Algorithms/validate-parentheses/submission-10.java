60a499ef3f81fba006f47aaedfbf037a21a1f89a
Sankalp Chaudhari <chaudharisankalp35@gmail.com>
2026-07-15 00:12:23 +0530

    Add: validate-parentheses - submission-10

---
+++ Data Structures & Algorithms/validate-parentheses/submission-10.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character,Character> map=new HashMap<>();
+        map.put(')','(');
+        map.put('}','{');
+        map.put(']','[');
+        Stack<Character> stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+            if(!map.containsKey(ch)){
+                stack.push(ch);
+            }
+            else{
+                if(stack.isEmpty() || map.get(ch)!=stack.peek()){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
