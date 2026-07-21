c19d50c252895d35ddefab2d809dbb9442715cab
Sankalp Chaudhari <chaudharisankalp35@gmail.com>
2026-07-21 16:54:30 +0530

    Add: eating-bananas - submission-13

---
+++ Data Structures & Algorithms/eating-bananas/submission-13.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+       int left=1;
+       int right=0;
+       for(int pile:piles){
+        right=Math.max(pile,right);
+       }
+       int ans=right;
+       while(left<=right){
+        int mid= left+(right-left)/2;
+        int hour=0;
+        for(int pile:piles){
+            hour+=(pile+mid-1)/mid;
+        }
+        if(hour<=h){
+            ans=mid;
+            right=mid-1;
+        }else left=mid+1;
+       }
+       return ans;
+    }
+}
